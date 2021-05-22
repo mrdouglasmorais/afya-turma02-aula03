9f29d615aa7a2bc8d3c69b922eb1fd2e7c1308d5
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-05-22 15:41:08 -0300

    Form sign configure

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
+
+interface IUserRegister {
+  nome: string;
+  usuario: string;
+  senha: string;
+}
 
 const FormSignUp: React.FC = () => {
+
+  const [formDataContent, setFormDataContent] = useState<IUserRegister>({} as IUserRegister);
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setIsLoad(true)
+      
+    }, []
+  );
   return (
     <div>
-      <form>
-        <input type="text" name="name" placeholder="Insira seu nome" />
-        <input type="text" name="username" placeholder="Insira seu nome de usuário" />
-        <input type="password" name="password" placeholder="Insira sua senha" />
-        <input type="submit" value="Criar conta" />
-      </form>
+      { isLoad ? (
+        <p>Carregando</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Insira seu nome"
+            onChange={e => setFormDataContent({ ...formDataContent, nome: e.target.value })}
+          />
+          <input
+            type="text"
+            name="username"
+            placeholder="Insira seu nome de usuário"
+            onChange={e => setFormDataContent({ ...formDataContent, usuario: e.target.value })}
+
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Insira sua senha"
+            onChange={e => setFormDataContent({ ...formDataContent, senha: e.target.value })}
+          />
+          <input
+            type="submit"
+            value="Criar conta"
+          />
+        </form>
+      )}
     </div>
   );
 }
