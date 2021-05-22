7abf44816721618153e52d3e8bbea08690e6d64c
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-05-22 18:52:39 -0300

    Private route include validate

--- src/private.routes.tsx
+++ src/private.routes.tsx
@@ -1,10 +1,30 @@
 import { Route, Redirect } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
 
 const PrivateRoutes: any = ({ component: Component, path: Path, ...rest}: any) => {
   const isLogin: string | null = localStorage.getItem('@tokenAfyaApp');
+
+  const isSectionActive: any = () => {
+    if ( isLogin === null ){
+      return false
+    } else {
+      const onlyToken: any = isLogin?.split(' ')[1]
+      const tokenPayLoad : any = jwt.decode(onlyToken)
+      const expSeconds = tokenPayLoad.exp;
+      const timeNow = Date.now() / 1000;
+  
+      return timeNow > expSeconds ? false : true
+
+    }
+  } 
+
+
+  console.log(isSectionActive())
+
+
   return (
     <Route {...rest} render={ props => (
-      isLogin !== null ? <Component {...props}/> : <Redirect to="/login"/>
+      isSectionActive() ? <Component {...props}/> : <Redirect to="/login"/>
     )}/>
   );
 }
